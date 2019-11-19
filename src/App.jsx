7d6c1e3987d4a871d2d8e0059bb3ea7793fe7480
Márcio Santos <marcio.tbms@gmail.com>
2019-11-18 21:00:38 -0300

    Creating theme and adding colors palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
 
 const App = () => {
   return (
     <>
-      <div>Header</div>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <div>Header</div>
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/theme/theme.js
@@ -0,0 +1,46 @@
+const white = {
+  default: "#FFFFFF"
+};
+
+const black = {
+  default: "#333333"
+};
+
+const orange = {
+  default: "#FF6A17"
+};
+
+const green = {
+  default: "#51C99C"
+};
+
+const brown = {
+  default: "#835826"
+};
+
+const blue = {
+  50: "#DFECFF",
+  100: "#B9D0EF",
+  200: "#9EB8DC",
+  500: "#4480C5",
+  700: "#1D5297",
+  900: "#16437E",
+  default: "#4480C5"
+};
+const gray = {
+  100: "#F1F6FB",
+  200: "#ECECEC",
+  500: "#B4C6D1",
+  700: "#8A8A8A",
+  default: "##8A8A8A"
+};
+
+export const colors = {
+  white,
+  black,
+  orange,
+  green,
+  brown,
+  blue,
+  gray
+};
